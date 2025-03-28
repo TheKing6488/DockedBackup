018f1bfbf811a4f4aa4384f56cf713b534ebde7b
The King <99882615+TheKing6488@users.noreply.github.com>
2025-03-28 21:50:04 +0100

    fix: missing files

---
+++ KopiaBackup/KopiaBackup.Lib/Models/Kopia/MigrateCredentialsStore.cs
@@ -0,0 +1,10 @@
+﻿namespace KopiaBackup.Lib.Models.Kopia;
+public record MigrateCredentialsStore
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public  required string Name { get; init; }
+    public required string SourceConfig { get; init; }
+    public required string ConfigFile { get; init; }
+    public required string Password { get; init; }
+
+}
