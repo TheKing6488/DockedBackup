f423053fc6b5b47b99b3e2406431bf6205142da3
The King <tobiaskonig75@gmail.com>
2025-05-01 16:45:56 +0200

    chore: port

--- src/DockedBackup.Tests/Test.cs
+++ src/DockedBackup.Tests/Test.cs
@@ -1,41 +1,23 @@
-﻿// using CommandLine;
-// using Moq;
-// using Shouldly;
-//
-//
-// namespace KopiaBackup.Console.Tests;
-//
-// public class Test
-// {
-//     [Fact]
-//     public void RunCreateExternalS3Config_WithValidArguments_ReturnsZero()
-//     {
-//         // Arrange: set up the command line arguments.
-//         var args = new[]
-//         {
-//             "connect",
-//             "-t", "0",
-//             "-b", "asdf",
-//             "-e", "asdf",
-//             "-a", "asdf",
-//             "-s", "asdf",
-//             "-p", "asdf",
-//             "-c", "asdf"
-//         };
-//
-//         // Create a mock for IKopiaHelper.
-//         var mockHelper = new Mock<IKopiaHelper>();
-//         // Optionally set up expectations:
-//         // mockHelper.Setup(h => h.DoSomething()).Verifiable();
-//
-//         // Act: Parse the arguments and use MapResult to call our command method.
-//         int exitCode = Parser.Default.ParseArguments<KopiaRepositoryConnect>(args)
-//             .MapResult(
-//                 options => KopiaCommands.RunCreateExternalS3Config(mockHelper.Object, options),
-//                 errors => 1  // Return 1 in case of parse errors.
-//             );
-//
-//         // Assert: Expect the exit code to be 0.
-//         exitCode.ShouldBe(0);
-//     }
-// }
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+public class ShellInCustomContainerTests : IClassFixture<CustomImageFixture>
+{
+    private readonly CustomImageFixture _fixture;
+
+    public ShellInCustomContainerTests(CustomImageFixture fixture)
+        => _fixture = fixture;
+
+    [Fact]
+    public async Task Should_Run_Echo_Command_Inside_Custom_Image()
+    {
+        var cmd = new[] { "sh", "-c", "echo hello_from_custom_image" };
+        var result = await _fixture.Container.ExecAsync(cmd).ConfigureAwait(false);
+
+        result.ExitCode.ShouldBe(0);
+        result.Stdout.Trim().ShouldBe("hello_from_custom_image");
+    }
+}
+
+
